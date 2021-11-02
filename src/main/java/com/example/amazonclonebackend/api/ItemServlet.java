d3a4833763f82fbf4d2838ff39145366dd3a0a61
cisco <sovisrushain@gmail.com>
2021-11-02 11:59:57 +0530

    Implement doGet method to fetch the data from DB

--- src/main/java/com/example/amazonclonebackend/api/ItemServlet.java
+++ src/main/java/com/example/amazonclonebackend/api/ItemServlet.java
@@ -3,8 +3,13 @@ package com.example.amazonclonebackend.api;
 import java.io.*;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
+import com.example.amazonclonebackend.dto.ItemDTO;
+import com.example.amazonclonebackend.service.ItemService;
 import jakarta.annotation.Resource;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -29,7 +34,16 @@ public class ItemServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
+        try(Connection connection = dataSource.getConnection()){
+            ItemService itemService = new ItemService(connection);
+            List<ItemDTO> items = itemService.getAllItems();
+            Jsonb jsonb = JsonbBuilder.create();
+            resp.setContentType("application/json");
+            resp.getWriter().println(jsonb.toJson(items));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to obtain a new connection");
+        }
     }
 
     public void destroy() {
