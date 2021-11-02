4e8bb771c5bf91b3cf1b8c7249061d0e29e3d07e
cisco <sovisrushain@gmail.com>
2021-11-02 11:34:38 +0530

    Update db-script.sql

--- src/db-script.sql
+++ src/db-script.sql
@@ -7,3 +7,16 @@ CREATE TABLE item(
                      unit_price DECIMAL(5,2) NOT NULL,
                      description VARCHAR(1000) NOT NULL
 );
+
+INSERT INTO item VALUES ('I001', 'PlayStation 5',
+                         'https://m.media-amazon.com/images/I/31C4szfCc-L._AC_UY218_.jpg',
+                         5,
+                         10,
+                         400,
+                         '<div>The <b>PlayStation 5</b> (PS5) is a home video game console
+                            developed by Sony Interactive Entertainment. " +
+                            "The Digital Edition lacks this drive, " +
+                            "allowing it to serve as a lower-cost model for those who prefer
+                            to buy games through digital download. " +
+                            "The two variants were launched simultaneously.</div>`');
+
