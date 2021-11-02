3dc37e7b0e772ac65d77ed4b5647412e40474afe
cisco <sovisrushain@gmail.com>
2021-11-02 10:49:15 +0530

    Add db-cript file

---
+++ src/db-script.sql
@@ -0,0 +1,9 @@
+CREATE TABLE item(
+                     code VARCHAR(10) PRIMARY KEY ,
+                     title VARCHAR(255) NOT NULL,
+                     image VARCHAR(500) NOT NULL,
+                     rating ENUM('1','2','3','4','5') NOT NULL,
+                     qty INT NOT NULL,
+                     unit_price DECIMAL(5,2) NOT NULL,
+                     description VARCHAR(1000) NOT NULL
+);
